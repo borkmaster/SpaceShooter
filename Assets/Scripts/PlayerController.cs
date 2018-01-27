0338c146fb3287defd14e73954e32460556bde6d
unluckymattress <unluckymattress@gmail.com>
2018-01-26 20:00:22 -0600

    Added Player Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float playerSpeed;
+	public float tiltAmount; // ~ 1800f is a full rotation with this setup
+	public float tiltSpeed;
+	public Boundary boundary;
+
+	void FixedUpdate ()
+	{
+		// Controls and restricts player movement without allowing any elastic movement past clamp limit
+		Rigidbody playerRigidbody = GetComponent<Rigidbody>();
+		float moveHorizontal = Input.GetAxis ("Horizontal") * playerSpeed * Time.deltaTime;
+		float moveVertical = Input.GetAxis ("Vertical") * playerSpeed * Time.deltaTime;
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		playerRigidbody.velocity = movement;
+		
+		playerRigidbody.position = new Vector3 
+			(
+				Mathf.Clamp (playerRigidbody.position.x + moveHorizontal, boundary.xMin, boundary.xMax),
+				0.0f, 
+				Mathf.Clamp (playerRigidbody.position.z + moveVertical, boundary.zMin, boundary.zMax)
+			);
+
+
+	}
+} 
